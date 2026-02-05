eb3dc1f568951ccae0a3ec1915a6c40ecea54959
nachinGirid <64187405+nachinGirid@users.noreply.github.com>
2026-02-05 16:06:35 -0500

    Refactor Seurat preprocessing script and save outputs
    Refactor input variable names and streamline preprocessing steps for RNA and ATAC data. Save filtered and preprocessed Seurat objects to output directory.

--- Dimension_reduction_methods_code/Seurat/01_preprocess_seurat_objects.R
+++ Dimension_reduction_methods_code/Seurat/01_preprocess_seurat_objects.R
@@ -1,56 +1,45 @@
 #!/usr/bin/env Rscript
 
 suppressPackageStartupMessages({
-  library(Matrix)
   library(Seurat)
   library(Signac)
 })
 
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) < 3) {
-  stop(
-    "Usage: 01_preprocess_seurat_objects.R <rna_counts_rds> <atac_counts_rds> <out_dir>\n",
-    "  rna_counts_rds: dgCMatrix genes x cells\n",
-    "  atac_counts_rds: dgCMatrix peaks x cells\n",
-    "  out_dir: output folder\n",
-    sep = ""
-  )
+  stop("Usage: 01_preprocess_seurat_objects.R <rna_counts_rds> <atac_counts_rds> <out_dir>")
 }
 
-rna_path  <- args[1]
-atac_path <- args[2]
-out_dir   <- args[3]
+rna_inpath  <- args[1]
+atac_inpath <- args[2]
+out_dir     <- args[3]
 dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
 
-rna_counts  <- readRDS(rna_path)
-atac_counts <- readRDS(atac_path)
+rna.data  <- readRDS(rna_inpath)
+atac.data <- readRDS(atac_inpath)
 
-stopifnot(inherits(rna_counts, "dgCMatrix") || is.matrix(rna_counts))
-stopifnot(inherits(atac_counts, "dgCMatrix") || is.matrix(atac_counts))
+rna  <- CreateSeuratObject(counts = rna.data)
+atac <- CreateSeuratObject(counts = atac.data)
 
-rna  <- CreateSeuratObject(counts = rna_counts, assay = "RNA")
-atac <- CreateSeuratObject(counts = atac_counts, assay = "peaks")
+saveRDS(rna,  file.path(out_dir, "seurat_rna_obj.rds"))
+saveRDS(atac, file.path(out_dir, "seurat_atac_obj.rds"))
 
-# Optional: remove all-zero features
-rna  <- rna[rowSums(rna@assays$RNA@counts) > 0, ]
-atac <- atac[rowSums(atac@assays$peaks@counts) > 0, ]
+atac_fil <- atac[rowSums(atac) != 0, ]
+rna_fil  <- rna[rowSums(rna) != 0, ]
 
-# RNA preprocessing
-DefaultAssay(rna) <- "RNA"
-rna <- NormalizeData(rna, verbose = FALSE)
-rna <- FindVariableFeatures(rna, verbose = FALSE)
-rna <- ScaleData(rna, verbose = FALSE)
-rna <- RunPCA(rna, verbose = FALSE)
+saveRDS(rna_fil,  file.path(out_dir, "Seurat_obj_rna_fil_rowsum0.rds"))
+saveRDS(atac_fil, file.path(out_dir, "Seurat_obj_atac_fil_rowsum0.rds"))
 
-# ATAC preprocessing (TF-IDF + LSI)
-DefaultAssay(atac) <- "peaks"
+# RNA preprocess
+rna <- NormalizeData(rna)
+rna <- FindVariableFeatures(rna)
+rna <- ScaleData(rna)
+rna <- RunPCA(rna)
+
+# ATAC preprocess
 atac <- RunTFIDF(atac)
 atac <- FindTopFeatures(atac, min.cutoff = "q0")
-atac <- RunSVD(atac)  # creates 'lsi'
+atac <- RunSVD(atac)
 
-saveRDS(rna,  file.path(out_dir, "pbmc_preprocessed_seurat_obj_rna.rds"))
-saveRDS(atac, file.path(out_dir, "pbmc_preprocessed_seurat_obj_atac.rds"))
-
-cat("Saved:\n",
-    " - ", file.path(out_dir, "pbmc_preprocessed_seurat_obj_rna.rds"), "\n",
-    " - ", file.path(out_dir, "pbmc_preprocessed_seurat_obj_atac.rds"), "\n", sep = "")
+saveRDS(rna,  file.path(out_dir, "Seurat_preprocess_rna_obj.rds"))
+saveRDS(atac, file.path(out_dir, "Seurat_preprocess_atac_obj.rds"))
