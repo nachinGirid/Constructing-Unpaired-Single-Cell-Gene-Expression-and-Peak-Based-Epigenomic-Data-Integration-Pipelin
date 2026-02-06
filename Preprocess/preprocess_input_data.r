079e7691264b0722d01dbb32512892a9b89bc637
nachinGirid <64187405+nachinGirid@users.noreply.github.com>
2026-02-06 12:10:05 -0500

    Add preprocessing script for RNA and ATAC data

---
+++ Preprocess/preprocess_input_data.r
@@ -0,0 +1,61 @@
+library(dplyr)
+library(Seurat)
+library(Signac)
+library(SingleCellExperiment)
+library(zellkonverter)
+
+# ---- load raw file ----
+counts<-Read10X_h5("filtered_feature_bc_matrix.h5")
+
+####### or ####
+#counts<-ReadMtx(
+#  mtx="matrix.mtx",
+#  cells="barcodes.tsv",
+#  features="features.tsv")
+##################
+
+rna=counts$'Gene Expression'
+atac=counts$'Peaks'
+# ---- save raw dgCMatrix ----
+saveRDS(rna, "rna_raw_dgCMatrix.rds"))
+saveRDS(atac, "atac_raw_dgCMatrix.rds"))
+
+# ---- load raw dgCMatrix ----
+rna_mat  <- readRDS("rna_raw_dgCMatrix.rds")  
+atac_mat <- readRDS("atac_raw_dgCMatrix.rds") 
+
+# ---- Seurat objects ----
+rna  <- CreateSeuratObject(counts = rna_mat, min.cells = 3)
+atac <- CreateSeuratObject(counts = atac_mat, assay = "ATAC", min.cells = 3)
+
+# ---- save raw Seurat ----
+saveRDS(rna, "seurat_rna_obj.rds")
+saveRDS(atac, "seurat_atac_obj.rds")
+
+
+# ---- SCE + H5AD ----
+# ---- filter rows with zero counts ----
+rna_keep  <- Matrix::rowSums(GetAssayData(rna,  slot = "counts"))  > 0
+atac_keep <- Matrix::rowSums(GetAssayData(atac, slot = "counts")) > 0
+rna_fil   <- subset(rna,  features = rownames(rna)[rna_keep])
+atac_fil  <- subset(atac, features = rownames(atac)[atac_keep])
+RNA_sce  <- as.SingleCellExperiment(rna_fil)
+ATAC_sce <- as.SingleCellExperiment(atac_fil)
+zellkonverter::writeH5AD(RNA_sce, "rna_fil_rowsum0.h5ad")
+zellkonverter::writeH5AD(ATAC_sce, "atac_fil_rowsum0.h5ad")
+
+# ---- independent preprocessing ----
+# RNA
+rna <- NormalizeData(rna, verbose = FALSE)
+rna <- FindVariableFeatures(rna, selection.method = "vst", nfeatures = 2000, verbose = FALSE)
+rna <- ScaleData(rna, features = rownames(rna), verbose = FALSE)
+rna <- RunPCA(rna, features = VariableFeatures(rna), verbose = FALSE)
+
+# ATAC
+DefaultAssay(atac) <- "ATAC"
+atac <- RunTFIDF(atac)
+atac <- FindTopFeatures(atac, min.cutoff = "q0")
+atac <- RunSVD(atac)
+
+saveRDS(rna, "Seurat_preprocess_rna_obj.rds")
+saveRDS(atac,  "Seurat_preprocess_atac_obj.rds")
